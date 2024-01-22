75f0922844b96dee9aad827459b4b06c8ffc1cf9
Nauman Chaudhry <naumanch969@gmail.com>
2024-01-22 22:15:01 +0500

    error-fixing

--- actions/get-total-revenure.tsx
+++ actions/get-total-revenure.tsx
@@ -1,4 +1,5 @@
 import prismadb from "@/lib/prisma"
+import { Order } from "@prisma/client"
 
 export const getTotalRevenure = async (storeId: string) => {
     const paidOrders = await prismadb.order.findMany({
@@ -10,8 +11,8 @@ export const getTotalRevenure = async (storeId: string) => {
         },
     })
 
-    const totalRevenue = paidOrders.reduce((total, order) => {
-        const orderTotal = order.orderItems.reduce((orderSum, item) => { return orderSum + item.product.price.toNumber() }, 0)
+    const totalRevenue = paidOrders.reduce((total: number, order) => {
+        const orderTotal = order.orderItems.reduce((orderSum: number, item) => { return orderSum + item.product.price.toNumber() }, 0)
         return total + orderTotal
     }, 0)
 
