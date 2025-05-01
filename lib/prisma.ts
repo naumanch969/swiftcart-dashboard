3970f21da852ecafbcdae4ab5e3b82d1482c97c2
naumanch969 <naumanch969@gmail.com>
2025-05-01 14:11:50 +0500

    prisma.ts update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+declare global {
+    var prisma: PrismaClient | undefined;
+}
+
+const prisma = globalThis.prisma || new PrismaClient()
+
+if (process.env.NEXT_ENV != "production") globalThis.prisma = prisma; // to prevent hot reloading which nextjs does support
 
 export default prisma
